227d45c285d8a000f4e92807d24bbb4d19a63f5d
vikstasenkova <viktoria_stasenkova@mail.ru>
2022-05-11 18:10:45 +0300

    feat: change language

--- index.js
+++ index.js
@@ -14,6 +14,43 @@ const Keyboard = {
         capsLock: false
     },
 
+    languageNow: "en",
+    keyArray: null,
+    
+    languages: {
+        en:  [
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+            "capslock", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+            "shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+            "alt", "space"
+            ],
+        ru: [
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+            "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ",
+            "capslock", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "enter",
+            "shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю",
+            "alt", "space"
+        ]
+    },
+
+    changeLanguage() {
+        console.log("отработал changeLanguage");
+    
+        if(Keyboard.languageNow === "en"){
+            keyArray = Keyboard.languages.ru;
+            console.log(keyArray);
+            Keyboard.languageNow = "ru";
+            console.log(Keyboard.languageNow + "nononno");
+        }
+        else{
+            keyArray = Keyboard.languages.en;
+            Keyboard.languageNow = "en"
+        }
+        console.log(Keyboard.languageNow + "lalala");
+        Keyboard.init();
+    },
+
     init() {
         this.elements.main = document.createElement("div");
         this.elements.keysContainer = document.createElement("div");
@@ -36,20 +73,20 @@ const Keyboard = {
 
     _createKeys() {
         const fragment = document.createDocumentFragment();
-        const keyLayout = [
-            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-            "capslock", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-            "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
-            "space"
-        ];
+
         const createIconHTML = (icon_name) => {
             return `<i class="material-icons">${icon_name}</i>`;
         };
+        if(this.languageNow === "en"){
+            keyArray = this.languages.en;
+        }
+        if(this.languageNow === "ru"){
+            keyArray = this.languages.ru
+        }
 
-        keyLayout.forEach(key => {
+        keyArray.forEach(key => {
             const keyElement = document.createElement("button");
-            const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+            const insertLineBreak = ["backspace", "p", "enter", "?", "ъ", "ю"].indexOf(key) !== -1;
             keyElement.setAttribute("type", "button");
             keyElement.setAttribute("data-key", `k${key}`);
             keyElement.classList.add("keyboard__key");
@@ -70,8 +107,7 @@ const Keyboard = {
                     keyElement.classList.add("keyboard__key--wide", "keyboard__key--activatable");
                     keyElement.innerHTML = createIconHTML("keyboard_capslock");
 
-                    console.log(keyElement);
-                    keyElement.addEventListener("keypress", () =>{
+                    keyElement.addEventListener("keydown", () =>{
                         console.log("dfgdgdgdg");
                     })
 
@@ -121,7 +157,7 @@ const Keyboard = {
                 fragment.appendChild(document.createElement("br"));
             }
         });
-
+            
         return fragment;
     },
 
@@ -132,8 +168,11 @@ const Keyboard = {
     },
 
     _toggleCapsLock() {
+        console.log(this.properties.capsLock);
         this.properties.capsLock = !this.properties.capsLock;
+        console.log(this.properties.capsLock);
         for (const key of this.elements.keys) {
+            console.log( this.elements.keys);
             if (key.childElementCount === 0) {
                 key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
             }
@@ -144,25 +183,42 @@ const Keyboard = {
         this.properties.value = initialValue || "";
         this.eventHandlers.oninput = oninput;
     },
-
-
 };
 
+// Animation into press
 document.addEventListener('keydown', function(){
+    console.log("Press key into Animation into press");
     activeTexteria();
     let currentButton = document.querySelector((`[data-key= K${event.key}]`).toLowerCase());
     if(!currentButton){
         currentButton = document.querySelector((`[data-key= kspace]`).toLowerCase());
     }
     currentButton.classList.add("active");
+    console.log(currentButton);
     setTimeout(() => currentButton.classList.remove("active"), 400);
 });
 
+// Focus
 function activeTexteria (){
     const inputKey = document.querySelector(".use-keyboard-input");
     inputKey.focus();
 }
 
+// Creating alt shift
+document.onkeydown = function(event) {
+    if (event.code == "AltLeft" || event.code == "AltRight") {
+        document.onkeyup = function (event) {
+            
+            if (event.code == "ShiftLeft" || event.code == "ShiftRight"){
+                console.log("отработал alt shift");
+                Keyboard.changeLanguage();
+            }
+            else {
+                document.onkeyup = null;
+            }
+        }
+    }
+}
 
 // Creating keyboard
 window.addEventListener("DOMContentLoaded", function () {
